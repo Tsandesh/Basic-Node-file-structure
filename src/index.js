5f0e42064d1074378f81351538a1f2b7dae2976f
Tsandesh <uchihazoro02@gmail.com>
2022-12-26 20:25:18 +0545

    Basic Node File Structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import connection from "./models/index.js"
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("BackEnd is working");
+})
+
+app.listen(process.env.PORT || 8000, async()=>{
+    console.log("SERVER STARTED 🚀");
+    try {
+        // await connection.authenticate();
+        // connection.sync();
+        console.log("Successfully connected to DB");
+    } catch (err) {
+        console.error("Error during connection to database ", err);
+    }
+})
